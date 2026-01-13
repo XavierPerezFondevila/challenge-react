fa6d49f1a12caf8d803a1e09cdc0bf3b1080b4df
Xavi Perez Fondevila <xaviperez1rb@gmail.com>
2026-01-13 19:54:21 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
-"use client";
-
 import PageTitle from "@/components/PageTitle";
 import SearchBar from "@/components/SearchBar";
 import CharactersGrid from "@/features/characters/CharactersGrid";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex min-h-screen w-full max-w-3xl flex-col gap-6 bg-white px-4 md:px-12 pb-8">
-        <div className="w-full mt-6 md:mt-12 flex flex-col gap-6">
-          <PageTitle />
-          <SearchBar />
-        </div>
-        <CharactersGrid />
+        <Suspense>
+          <div className="w-full mt-6 md:mt-12 flex flex-col gap-6">
+            <PageTitle />
+            <SearchBar />
+          </div>
+          <CharactersGrid />
+        </Suspense>
       </main>
     </div>
   );
