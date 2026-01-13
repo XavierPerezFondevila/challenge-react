08451ec1d644ebe7dbc031e08f3804a892ad7b82
Xavi Perez Fondevila <xaviperez1rb@gmail.com>
2026-01-13 19:56:18 +0100

    Update CharacterCard.tsx

--- features/characters/CharacterCard.tsx
+++ features/characters/CharacterCard.tsx
@@ -13,16 +13,17 @@ interface CharacterCardProps {
 const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
 
   return (
-    <Link href={`/characters/${character.id}`}>
-      <div className="flex flex-col items-center max-w-[189px] bg-primary group overflow-hidden">
-        <div className="relative w-full">
-          <img
-            src={character?.image?.small_url}
-            alt={character?.name}
-            className="object-cover object-top aspect-square w-full"
-          />
-          <div className="absolute bottom-0 left-0 w-full h-1.25 bg-secondary"></div>
-        </div>
+    <div className="flex flex-col items-center max-w-[189px] bg-primary group overflow-hidden">
+        <Link href={`/characters/${character.id}`}>
+          <div className="relative w-full">
+            <img
+              src={character?.image?.small_url}
+              alt={character?.name}
+              className="object-cover object-top aspect-square w-full"
+            />
+            <div className="absolute bottom-0 left-0 w-full h-1.25 bg-secondary"></div>
+          </div>
+        </Link>
 
         <div className="relative w-full min-h-13 p-4 pb-6 overflow-hidden">
           <div className="absolute top-0 left-0 w-full h-0.5 bg-secondary transition-all duration-375 group-hover:h-full z-10"></div>
@@ -35,7 +36,6 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
           <div className="absolute bottom-0 right-0 w-0 h-0 inline-block w-0 h-0 border-solid border-t-0 border-r-0 border-l-12 border-b-12 border-l-transparent border-r-transparent border-t-transparent border-white z-30"></div>
         </div>
       </div>
-    </Link>
   );
 };
 
